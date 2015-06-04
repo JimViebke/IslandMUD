32d3891f5f471d45ffea9e8293805181f042b3af
Jim Viebke <jviebke@gmail.com>
2015-06-04 10:04:34 -0400

    tweaking
    No functional changes

--- IslandMUD-server/recipe.h
+++ IslandMUD-server/recipe.h
@@ -11,21 +11,22 @@ using namespace std;
 
 class Recipe
 {
+private:
+	Recipe() = delete; // this is deliberately not implemented
+
 public:
 	map<string, int> inventory_need; // needs to be in inventory
 	map<string, int> inventory_remove; // needs to be in inventory, is removed
-	map<string, int> local_need; // needs to be in the node
-	map<string, int> local_remove; // needs to be in the node, is removed
+	map<string, int> local_need; // needs to be in the room
+	map<string, int> local_remove; // needs to be in the room, is removed
 
 	map<string, int> yields; // the item(s) the character recieves
-
-	Recipe() = delete; // This is deliberately not implemented
 
 	Recipe(
 		const map<string, int> & inv_n, const map<string, int> & inv_r,
 		const map<string, int> & loc_n, const map<string, int> & loc_r,
 		const map<string, int> & yeilds) :
-		inventory_need(inv_n), inventory_remove(inv_r), local_need(loc_n), local_remove(loc_r), yields(yeilds) { }
+		inventory_need(inv_n), inventory_remove(inv_r), local_need(loc_n), local_remove(loc_r), yields(yeilds) {}
 };
 
 #endif
