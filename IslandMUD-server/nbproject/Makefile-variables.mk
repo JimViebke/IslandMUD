67af7cef64c4e567f37d35302e9ee10cb7efd15c
Jim Viebke <jviebke@gmail.com>
2015-08-27 14:55:24 -0400

    add NetBeans project files

---
+++ IslandMUD-server/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=None-Windows
+CND_ARTIFACT_DIR_Debug=dist/Debug/None-Windows
+CND_ARTIFACT_NAME_Debug=islandmud-server
+CND_ARTIFACT_PATH_Debug=dist/Debug/None-Windows/islandmud-server
+CND_PACKAGE_DIR_Debug=dist/Debug/None-Windows/package
+CND_PACKAGE_NAME_Debug=islandmud-server.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/None-Windows/package/islandmud-server.tar
+# Release configuration
+CND_PLATFORM_Release=None-Windows
+CND_ARTIFACT_DIR_Release=dist/Release/None-Windows
+CND_ARTIFACT_NAME_Release=islandmud-server
+CND_ARTIFACT_PATH_Release=dist/Release/None-Windows/islandmud-server
+CND_PACKAGE_DIR_Release=dist/Release/None-Windows/package
+CND_PACKAGE_NAME_Release=islandmud-server.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Windows/package/islandmud-server.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
