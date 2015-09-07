8f7a0da4e8503772316299749532e32159f1ba0a
Jim Viebke <jviebke@gmail.com>
2015-09-07 00:04:45 -0400

    reduce view distance

--- IslandMUD-server/constants.cpp
+++ IslandMUD-server/constants.cpp
@@ -5,7 +5,7 @@ Feb 14, 2015 */
 
 // general
 const int C::GROUND_INDEX = 3; // there are three levels below this
-const int C::VIEW_DISTANCE = 10; // 5+1+5 to a side == 11*11 total area
+const int C::VIEW_DISTANCE = 9; // 5+1+5 to a side == 11*11 total area
 
 const int C::WORLD_X_DIMENSION = 1000;
 const int C::WORLD_Y_DIMENSION = 1000;
