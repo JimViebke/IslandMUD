af4c5f0342111b3584b879771ec1dc90c6d4cd29
Jim Viebke <jviebke@gmail.com>
2015-11-15 15:54:07 -0500

    documentation

--- IslandMUD/threadsafe_queue.h
+++ IslandMUD/threadsafe_queue.h
@@ -1,28 +1,7 @@
 
-// Created by Samvel Khalatyan on Apr 28, 2014
-// Copyright (c) 2014 Samvel Khalatyan. All rights reserved
-//
-// The MIT License(MIT)
-//
-// Copyright(c) 2014 Samvel Khalatyan
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files(the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions :
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
+/* Jim Viebke
+
+Based on Samvel Khalatyan's "queue" released the MIT license. See the bottom of this file. */
 
 #ifndef THREADSAFE_QUEUE_H
 #define THREADSAFE_QUEUE_H
@@ -36,13 +15,6 @@ namespace threadsafe
 {
 	template<typename T>
 	class queue
-		/*
-		* thread-safe queue
-		*
-		* It has one potential problem: the get will stall the
-		* consuming thread if producer stops adding elements, e.g.
-		* there are N + 1  calls to get(...) with N calls to put().
-		*/
 	{
 	public:
 		queue() {}
@@ -54,7 +26,7 @@ namespace threadsafe
 			_cv.notify_one();
 		}
 
-		void get(T & dest)
+		void get(T & dest) // blocking
 		{
 			std::unique_lock<std::mutex> lock(_mutex);
 			_cv.wait(lock, [this] { return !_queue.empty(); });
@@ -62,7 +34,7 @@ namespace threadsafe
 			_queue.pop();
 		}
 
-		T get()
+		T get() // blocking
 		{
 			std::unique_lock<std::mutex> lock(_mutex);
 			_cv.wait(lock, [this] { return !_queue.empty(); });
@@ -91,3 +63,25 @@ namespace threadsafe
 }
 
 #endif
+
+// The MIT License(MIT)
+//
+// Copyright(c) 2014 Samvel Khalatyan
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
