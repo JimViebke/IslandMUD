e422cdfa01e3c054d113ea8b4b4866e5df3d5a1e
Jim Viebke <jviebke@gmail.com>
2015-11-17 21:25:51 -0500

    add an optional flag to call for map updates

--- IslandMUD/message.h
+++ IslandMUD/message.h
@@ -35,13 +35,14 @@ public:
 	std::string to_user; // an update is always sent to the user
 	std::unique_ptr<std::string> to_room; // an update may be sent to all players in the room
 	std::unique_ptr<std::string> to_area; // an update may be sent to all players in the area
+	bool map_update_required;
 
-	explicit Update_Messages(const std::string & user_message) : to_user(user_message) {}
-	explicit Update_Messages(const std::string & user_message, const std::string & room_message) : to_user(user_message)
+	explicit Update_Messages(const std::string & user_message, const bool set_map_update = false) : to_user(user_message), map_update_required(set_map_update) {}
+	explicit Update_Messages(const std::string & user_message, const std::string & room_message, const bool set_map_update = false) : to_user(user_message), map_update_required(set_map_update)
 	{
 		to_room = U::make_unique<std::string>(room_message);
 	}
-	explicit Update_Messages(const std::string & user_message, const std::string & room_message, const std::string & area_message) : to_user(user_message)
+	explicit Update_Messages(const std::string & user_message, const std::string & room_message, const std::string & area_message, const bool set_map_update = false) : to_user(user_message), map_update_required(set_map_update)
 	{
 		to_room = U::make_unique<std::string>(room_message);
 		to_area = U::make_unique<std::string>(area_message);
