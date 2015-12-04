27fa8356275f05a975913302cb306355ad47851e
Jim Viebke <jviebke@gmail.com>
2015-12-03 23:00:09 -0500

    remove old utilities

--- IslandMUD/main.cpp
+++ IslandMUD/main.cpp
@@ -15,16 +15,6 @@ int main()
 	U::create_path_if_not_exists(C::room_directory);
 	U::create_path_if_not_exists(C::user_data_directory);
 
-	// write utilities to disk
-#ifdef _WIN32
-	U::to_file(C::game_directory + "/" + "move room data to delete folder.bat.txt",
-		std::string("move C:/IslandMUD/rooms C:/IslandMUD/delete"));
-	U::to_file(C::game_directory + "/" + "wipe delete folder.bat",
-		std::string("del /f/s/q C:/IslandMUD/delete > nul") + "\n" + "rmdir /s/q C:/IslandMUD/delete");
-#else
-	// add Linux equivalent?
-#endif
-
 	Game game;
 
 	// run the processing thread using the main thread
