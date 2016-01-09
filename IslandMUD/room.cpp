c3d600fc2a91e637454e6e7d80c7172a06db028f
Jim Viebke <jviebke@gmail.com>
2016-01-09 14:04:41 -0500

    add correct message for empty chests

--- IslandMUD/room.cpp
+++ IslandMUD/room.cpp
@@ -173,6 +173,10 @@ Update_Messages Room::chest_contents(const std::string & faction_ID, const std::
 		return Update_Messages("This chest has an unfamiliar lock.",
 			username + " tries the chest's lock.");
 	}
+	
+	// if the chest is empty
+	if (chest->size() == 0) return Update_Messages("There is nothing in the chest.",
+		username + " looks into the chest.");
 
 	// return the contents of the chest
 	return Update_Messages(chest->contents_to_string(), username + " looks into the chest.");
