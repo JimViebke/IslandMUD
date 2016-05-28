9cf64c557796d84e233473100292da5d8c0352b3
Jim Viebke <jviebke@gmail.com>
2016-05-28 17:04:08 -0400

    rename a file

---
+++ IslandMUD/NPC/npc_neutral.cpp
@@ -0,0 +1,4 @@
+/* Jim Viebke
+Jun 3 2015 */
+
+#include "npc_neutral.h"
--- IslandMUD/npc.h
+++ IslandMUD/npc.h
@@ -8,11 +8,13 @@ Include header for NPC types */
 #define NPC_H
 
 #include "NPC/non_player_character.h"
+
 #include "NPC/npc_enemy.h"
 #include "NPC/npc_enemy_bodyguard.h"
 #include "NPC/npc_enemy_corporal.h"
 #include "NPC/npc_enemy_fighter.h"
 #include "NPC/npc_enemy_worker.h"
-#include "NPC/npc_unaffiliated.h"
+
+#include "NPC/npc_neutral.h"
 
 #endif
