f69170a57b190b5895456b48672f18b829c2c3a0
Jim Viebke <jviebke@gmail.com>
2016-09-14 05:51:48 -0700

    minor re-org 2

--- IslandMUD/Threadsafe/threadsafe_socket_lookup.h
+++ IslandMUD/Threadsafe/threadsafe_socket_lookup.h
@@ -8,6 +8,18 @@ Nov 13, 2015 */
 #include <map>
 #include <mutex>
 
+#ifdef WIN32
+#define NOMINMAX // fix min() and max() errors
+#include <WinSock2.h>
+#include <Windows.h>
+#pragma comment (lib, "Ws2_32.lib")
+#else
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <cstring>
+#endif
+
 namespace threadsafe
 {
 	class socket_lookup
@@ -25,53 +37,23 @@ namespace threadsafe
 		mutable std::mutex mutex; // this mutex can be locked by constant member functions
 
 	public:
-		socket_lookup() {}
+		socket_lookup();
 
 		// save the user's socket
-		void set_socket(const std::string & user_ID, const SOCKET & socket)
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			sockets_map[user_ID].user_socket = socket;
-		}
+		void set_socket(const std::string & user_ID, const SOCKET & socket);
 		// save the user's map socket
-		void set_map_socket(const std::string & user_ID, const SOCKET & map_socket)
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			sockets_map[user_ID].map_socket = map_socket;
-		}
+		void set_map_socket(const std::string & user_ID, const SOCKET & map_socket);
 
 		// retrieve a user's socket
-		SOCKET get_socket(const std::string & user_ID) const
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			const auto it = sockets_map.find(user_ID);
-			return (it != sockets_map.cend()) ? it->second.user_socket : -1;
-		}
+		SOCKET get_socket(const std::string & user_ID) const;
 		// retrieve a user's map socket
-		SOCKET get_map_socket(const std::string & user_ID) const
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			const auto it = sockets_map.find(user_ID);
-			return (it != sockets_map.cend()) ? it->second.map_socket : -1;
-		}
+		SOCKET get_map_socket(const std::string & user_ID) const;
 
 		// retrieve a user ID associated with a socket (reverse lookup)
-		std::string get_user_ID(const SOCKET & socket) const
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			for (auto it = sockets_map.cbegin(); it != sockets_map.cend(); ++it)
-			{
-				if (it->second.user_socket == socket || it->second.map_socket == socket) return it->first;
-			}
-			return "";
-		}
+		std::string get_user_ID(const SOCKET & socket) const;
 
 		// erase the records for a user
-		void erase(const std::string & user_ID)
-		{
-			std::lock_guard<std::mutex> lock(mutex);
-			sockets_map.erase(user_ID);
-		}
+		void erase(const std::string & user_ID);
 	};
 }
 
