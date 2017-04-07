6c0a978c31bb360ff462f00050894db71329fb2e
Jim Viebke <jviebke@gmail.com>
2017-04-07 15:05:16 -0400

    fix documentation

--- IslandMUD/item/equipment.cpp
+++ IslandMUD/item/equipment.cpp
@@ -11,7 +11,7 @@ unsigned Equipment::get_combat_damage() const
 	// get the damage table for this item
 	const std::map<std::string, std::map<std::string, int>>::const_iterator implement_table_it = C::damage_tables.find(this->get_name());
 
-	// if no damage table exists for this item, return 0
+	// if no damage table exists for this item, return 1
 	if (implement_table_it == C::damage_tables.cend()) return 1;
 
 	// a damage table exists for this item, extract it from the iterator
@@ -20,7 +20,7 @@ unsigned Equipment::get_combat_damage() const
 	// get the damage amount against a character
 	const std::map<std::string, int>::const_iterator implement_damage = implement_table.find("");
 
-	// if a damage table exists for this item but does not have an entry for attacking a character, return 0
+	// if a damage table exists for this item but does not have an entry for attacking a character, return 1
 	if (implement_damage == implement_table.cend()) return 1;
 
 	// return the damage
